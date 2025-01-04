a8712283d7bdaf4b69357135a9fdaca521a8002d
HonmaMeikodesu <whonmameikom@gmail.com>
2025-01-04 22:59:33 +0800

    init

---
+++ docs/research/hentaiverse/2.js
@@ -0,0 +1,10 @@
+// hentaiverse 数据库 https://hv-monsterdb-data.skk.moe/persistent.json
+
+[
+{"monsterId":84361,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Elemental","monsterName":"Roc Executive Yuan","plvl":1360,"attack":"Cold","trainer":".@_@.@_@.","piercing":-25,"crushing":-25,"slashing":0,"cold":65,"wind":65,"elec":65,"fire":65,"dark":65,"holy":65,"lastUpdate":"2021-04-11"},
+{"monsterId":81535,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Arthropod","monsterName":"Touyama Nao","plvl":1431,"attack":"Piercing","trainer":"TurendoT","piercing":0,"crushing":-25,"slashing":25,"cold":33,"wind":64,"elec":64,"fire":64,"dark":48,"holy":48,"lastUpdate":"2021-04-17"},
+{"monsterId":112698,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Giant","monsterName":"Bbobbo","plvl":1138,"attack":"Crushing","trainer":"wannaf","piercing":-25,"crushing":25,"slashing":0,"cold":59,"wind":26,"elec":27,"fire":60,"dark":43,"holy":42,"lastUpdate":"2021-04-18"},
+{"monsterId":100125,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Dragonkin","monsterName":"Beta-gravis 00a","plvl":1104,"attack":"Piercing","trainer":"夕涼なのか","piercing":0,"crushing":25,"slashing":25,"cold":9,"wind":26,"elec":59,"fire":59,"dark":42,"holy":42,"lastUpdate":"2021-04-18"},
+{"monsterId":100146,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Giant","monsterName":"Charles Augustus Lindbergh","plvl":1104,"attack":"Crushing","trainer":"Zack_CN","piercing":-25,"crushing":25,"slashing":0,"cold":59,"wind":26,"elec":26,"fire":59,"dark":42,"holy":42,"lastUpdate":"2021-04-18"},
+{"monsterId":100147,"created_at":"2024-09-26T12:51:27.392462+00:00","monsterClass":"Giant","monsterName":"Walter Chrysler","plvl":1104,"attack":"Crushing","trainer":"Zack_CN","piercing":-25,"crushing":25,"slashing":0,"cold":59,"wind":26,"elec":26,"fire":59,"dark":42,"holy":42,"lastUpdate":"2021-04-18"},
+]
