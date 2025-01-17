763947de59475c678d0fd28609c2a6e4d0db3e78
HonmaMeikodesu <whonmameikom@gmail.com>
2025-01-17 15:36:25 +0800

    refine

---
+++ src/prompt-builder/src/battle-round/index.ts
@@ -0,0 +1,4 @@
+export function buildRoundStartPrompt() {
+    return `A new round is starting! A new group of monsters has been spawned. Below is 
+`
+}
--- src/prompt-builder/src/context-init/index.ts
+++ src/prompt-builder/src/context-init/index.ts
@@ -183,10 +183,22 @@ const battleSigrepDemo: BattleSigRep = {
 }
 
 export const systemInitPromptBuilder = () => `You are going to play a roleplaying game where you are a player in battles against monsters.
-The game is like pokemon battle, but with monsters instead of pokemon.In this game. A battle is consisted of multiple rounds. A round has a number of turns.
+The game is like pokemon battle, but with monsters instead of pokemon. In this game, a battle is consisted of multiple rounds. A round has a number of turns.
+Turns are divided into player turns and monsters turns, for player turn you can only execute one action, for monsters turn, all monsters attack you simultaneously, then gets back to next player turn, making it a interaction loop.
+Because you can't do anything in monsters turn, from here on now we only focus on player turn, hence the follwing will abbreviate the player turn as "turn" only.
+Victory Condition: You win the battle if you eliminate(doing harm to make monster HP drop to 0) all the monsters in all rounds of a battle.
+Defeat Condition: Whenever your HP drops to 0, you are defeated and the battle is over immediately.
+
+If you receive the prompt **You encouter a battle**, that marks the begin of a battle
 For each battle, player will be initialized with full amount of HP, MP and SP.
+
+If you receive the prompt **A new round is starting**, that marks the begin of a new round.
 For each round, a new group of monsters will be spawned. But player will inherit the HP, MP and SP from the previous round.
-For each turn, player will be given a list of actions that they can perform. An action can be one of the following:
+Making all the monsters' HP drop to 0 marks the end of current round and a new round to begin. If no more rounds are left, the battle is over and you win the battle.
+Player will always get the first turn in a round to perform an action.
+
+If you receive the prompt **Time for a new turn**, that marks a new turn.
+For each turn, player will have to perform an action to fight or flight. An action can be one of the following:
 - ${BattleAction.ATTACK}: player attacks a monster with their weapon
 - ${BattleAction.SKILL}: player uses a skill from their skill list. The full available skill list will be provided
 - ${BattleAction.SPELL}: player uses a spell from their spell list. The full available spell list will be provided
@@ -197,11 +209,11 @@ For each turn, player will be given a list of actions that they can perform. An 
 
 For each battle/round/turn to start, you, as the player, will be given different types of reports in JSON format to help you make decisions.
 Note that you will see some fields in the report which affixed with "%" percentage sign, which means the value is a percentage and every 100 represents "100%" as a whole.
-Detailed report format is depicted as follows.
+Detailed report format will be depicted in below section.
 
 ---
 
-For each battle, you will be given a player stats report
+For each battle start, you will be given a player stats report
 The player stats report is in following format:
 \`\`\`
 ${JSON.stringify(playerStatsDemo)}
@@ -209,7 +221,7 @@ ${JSON.stringify(playerStatsDemo)}
 
 ---
 
-For each round, since new monsters are spawned, you will be given a monsters stats report.
+For each round start, since new monsters are spawned, you will be given a new monsters stats report.
 The monsters stats report is in following format:
 \`\`\`
 ${JSON.stringify(monstersStatsDemo)}
@@ -218,7 +230,7 @@ ${JSON.stringify(monstersStatsDemo)}
 ---
 
 For each turn, You will be given a battle report from the game and you will be asked to analyze it and perform an action for next turn
-Note that a battle report includes the status quo for both player and monsters.Due to skills or spells, player will gain positive effects and monsters will gain negative effects.
+Note that a battle report includes the status quo for both player and monsters.Due to skills or spells, player might gain positive effects and monsters might gain negative effects.
 The battle report is in following format:
 \`\`\`
 ${JSON.stringify(battleSigrepDemo)}
@@ -226,9 +238,26 @@ ${JSON.stringify(battleSigrepDemo)}
 Note that the action player can perfrom is recored in the \`availableAction\` field, which is a map from action type to action name.
 
 ---
-
 `
 
-export const fightStyleInitPromptBuilder = () => `${BattleAction.SKILL}: As a ${FightStyle.OneHanded} style player, you should try to persist Spirit Stance as often as possible, which means you should try to avoid using weapon skills because they will consume your overcharge
-${BattleAction.SKILL}: persist ${RestorativeSlotItem.HealthDraught} buff all the time to maintain your HP a safe watermark. Use ${RestorativeSlotItem.ManaDraught} if your mana runs low. Use ${RestorativeSlotItem.SpiritDraught} only if you find your SP is about to run out, persisting Spirit Stance costs SP and overcharge continuously
-${BattleAction.SPELL}: persist ${[SupportiveSpell.Protection, SupportiveSpell.Heartseeker, SupportiveSpell.Regen].join(",")} buff all the time, because they provide defense and damage enhancement`
+export const fightStyleInitPromptBuilder = () => `There are also some useful tips or practice experience ranging form ${BattleAction.SKILL} usage, ${BattleAction.ITEMS} usage and ${BattleAction.SPELL} usage, provided to you to keep you more informed when deciding what actions to make for each turn.
+${BattleAction.SKILL}: As a ${FightStyle.OneHanded} style player, you should try to persist Spirit Stance as often as possible, which means you should try to avoid using weapon skills because they will consume your overcharge
+${BattleAction.ITEMS}: persist ${RestorativeSlotItem.HealthDraught} buff all the time to maintain your HP a safe watermark. Use ${RestorativeSlotItem.ManaDraught} if your mana runs low. Use ${RestorativeSlotItem.SpiritDraught} only if you find your SP is about to run out, persisting Spirit Stance costs SP and overcharge continuously
+${BattleAction.SPELL}: persist ${[SupportiveSpell.Protection, SupportiveSpell.Heartseeker, SupportiveSpell.Regen].join(",")} buff all the time, because they provide defense and damage enhancement
+
+---
+`
+
+export const evaluatePromptBuilder = () => `Before the game starts, i need you to evaluate first, from 0 to 10, how would you grade the system init prompts above? what critical information do you find missing?`
+
+export const roleInitPromptBuilder = () => `Now you are about to start the game, for next prompt a new realtime battle will approach, get ready!`
+
+export default function initPromopt() {
+    return {
+        systemInit: `${systemInitPromptBuilder()}
+${fightStyleInitPromptBuilder()}
+`,
+        roleInit: roleInitPromptBuilder(),
+        evaluate: evaluatePromptBuilder()
+    }
+}
